9090599aca0c382ee3b4a4120f6a0a7137a83cb7
Danish Belal <1886637+danishbelal@users.noreply.github.com>
2021-05-22 19:54:14 +0200

    Add docstrings

--- source/SlxUtils.hpp
+++ source/SlxUtils.hpp
@@ -8,9 +8,20 @@
 #include <string>
 #include <unordered_map>
 
+/// Determines the Simulink version that was used to create the file.
 std::string InferVersion(ZipArchive::Ptr ModelFile);
+
+/// Finds all relationship files in the Zip.
 std::set<ZipArchiveEntry::Ptr> FindRelationships(ZipArchive::Ptr ZipFile);
+
+/// Parses the XML File and returns the xml_document
 pugi::xml_document ParseXmlFile(ZipArchiveEntry::Ptr File);
+
+/// Finds all relationship files and accumulates their relationships
+/// with the corresponding targets.
+///
+/// The map key is the relationship id.
+/// The map value is the relationship target.
 std::unordered_map<std::string, std::string> FindRelTargets(ZipArchive::Ptr ZipFile);
 
 #endif
--- source/ZipUtils.hpp
+++ source/ZipUtils.hpp
@@ -7,8 +7,13 @@
 #include <set>
 #include <string>
 
+/// Iterates through all files contained in the ZipFile and attempts to match
+/// their filename stem against the regex.  Therefore possibly preceding paths
+/// are ignored.  All files which matched the regex are returned in the std::set.
 std::set<ZipArchiveEntry::Ptr> FindFiles(ZipArchive::Ptr ZipFile, const std::regex FilenameSelector);
 
+/// A wrapper for FindFiles(ZipArchive::Ptr, const std::regex).  This can be
+/// called with any string type which is then converted to an std::regex.
 template <typename StringT>
 std::set<ZipArchiveEntry::Ptr> FindFiles(ZipArchive::Ptr ZipFile, const StringT FilenameSelector)
 {
