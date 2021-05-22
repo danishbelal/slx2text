7c784566824c226d7fce4cd46e60cf19d5bbf1b0
Danish Belal <1886637+danishbelal@users.noreply.github.com>
2021-05-22 20:45:14 +0200

    Can dump any relationship target.  Needs cleanup.

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -5,6 +6,8 @@
 
 #include "SlxUtils.hpp"
 #include "ZipUtils.hpp"
+
+#include <toml++/toml.h>
 
 int main(int argc, char** argv)
 {
@@ -17,16 +20,58 @@ int main(int argc, char** argv)
     auto ModelFilename = argv[1];
     ZipArchive::Ptr Model = ZipFile::Open(ModelFilename);
 
+    // Does the config file exist?
+    std::filesystem::path ConfigFilePath("slx2text-config.toml");
+    if (!std::filesystem::exists(ConfigFilePath))
+    {
+        std::cerr << "Cannot find config file" << std::endl;
+        return -1;
+    }
+
+    // Read config
+    toml::table Config;
+    try
+    {
+        std::ifstream ConfigFileInputStream(ConfigFilePath);
+        Config = toml::parse(ConfigFileInputStream);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    // Extract list of relationship ids to be printed
+    std::set<std::string> ShowIds;
+    try
+    {
+        toml::array* RelIdList = Config["ids"].as_array();
+        for (toml::node& Id : *RelIdList)
+        {
+            Id.visit([&](auto&& e) {
+                if constexpr (toml::is_string<decltype(e)>)
+                {
+                    auto v = Id.value<std::string>().value();
+                    ShowIds.emplace(v);
+                }
+            });
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     try
     {
         IC(InferVersion(Model));
         auto TargetSet = FindRelTargets(Model);
 
-        auto Target = FindRelTargetById(TargetSet, "blockDiagram");
-        auto File = Model->GetEntry(Target);
-        DumpXmlFile(std::cout, File);
-
-        IC(TargetSet);
+        for (auto Id : ShowIds)
+        {
+            auto Target = FindRelTargetById(TargetSet, Id);
+            auto File = Model->GetEntry(Target);
+            DumpXmlFile(std::cout, File);
+        }
     }
     catch (const std::exception& e)
     {
